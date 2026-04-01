9a9b9d9b960d10c330d63bbe258db633dd35f5e8
Matheus <matheuscruz500@gmail.com>
2026-04-01 14:47:07 -0300

    firebase integration

--- app/(tabs)/hello.tsx
+++ app/(tabs)/hello.tsx
@@ -1,12 +1,74 @@
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, FlatList, ActivityIndicator } from 'react-native';
+import {db} from '../../firebaseConfig.js';
+import { collection , addDoc, getDocs } from 'firebase/firestore';
 
-import { ThemedText } from '@/components/themed-text';
+
+
+interface Entry {
+  id: string;
+  name: string;
+}
+
 export default function FireBaseTest() {
+  const [entries, setEntries] = useState<Entry[]>([]);
+  const [loading, setLoading] = useState(true);
 
+  const writeTest = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "test"), {
+        name: "junin da capitinga",
+        sapato: 48,
+        sabor: "energetico"
+      });
+
+      console.log("document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("deu ruim paizao", e);
+    }
+
+  }
+
+  const readTest = async () => {
+    try {
+      const query = await getDocs(collection(db,"test"));
+      const list: Entry[] = [];
+
+      query.forEach((doc) => {
+        list.push({
+        id: doc.id,
+        name: doc.data().name,
+        });  
+      });
+
+      setEntries(list);
+
+    } catch(e){
+      console.error("deu ruim", e)
+    } finally {
+      setLoading(false)
+    }
+
+    if(loading){
+      return <ActivityIndicator size="large"/>;
+    }
+
+  }
+  useEffect(() => {readTest();}, [] );
 	return (
-		<view>
-			<ThemedText> Hello world!</ThemedText>
-		</view>
+		<View>
+    <FlatList
+      data={entries}
+      keyExtractor={(item) => item.id}
+      renderItem={({item}) => (
+        <View>
+          <Text>{item.name}</Text>
+        </View>
+      )}
+      ListEmptyComponent={<Text>tem nada aqui nao meu mestre, aperta o botao.</Text>}
+    />
+		<Button title="envia pro firebase" onPress={writeTest}/>
+    </View>
 
 	);
 
