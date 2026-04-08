990d988f87d4bc1f8bf5a200a4beacbcb8d6300e
Matheus <matheuscruz500@gmail.com>
2026-04-08 21:01:12 +0200

    tela login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,117 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity,View, SafeAreaView, Button, FlatList, ActivityIndicator } from 'react-native';
+import {db} from '../../firebaseConfig.js';
+import { collection , addDoc, getDocs } from 'firebase/firestore';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+
+
+interface Entry {
+  id: string;
+  name: string;
+}
+
+export default function FireBaseTest() {
+  const [entries, setEntries] = useState<Entry[]>([]);
+  const [loading, setLoading] = useState(true);
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.header}>
+				<Ionicons name="menu" size={32}/>
+				<Text style={styles.header_text}>Login</Text>
+			</View>
+
+			<View style={{ height: 50 }} />
+			
+			<View style={styles.layout}>
+				<TextInput
+					style={styles.input}
+					placeholder="Nome de usuario"
+				/>
+				<TextInput 
+					style={styles.input}
+					placeholder="Senha"
+					secureTextEntry={true}
+				/>
+				<TouchableOpacity style={[styles.button, styles.button_entrar]}>
+					<Text>ENTRAR</Text>
+				</TouchableOpacity>
+				
+							
+				<TouchableOpacity style={[styles.button, styles.button_facebook]}>
+				<FontAwesome name="facebook-square" size={24} color="white" />
+					<Text style={{color: '#FFFFFF'}}>ENTRAR COM FACEBOOK</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={[styles.button, styles.button_google]}>
+					<FontAwesome name="google" size={24} color="white" />
+					<Text style={{color: '#FFFFFF'}}>ENTRAR COM GOOGLE</Text>
+				</TouchableOpacity>
+			</View>
+    </SafeAreaView>
+
+	);
+
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+},
+	header: {
+	flexDirection: 'row',
+	height: 75,
+	width: '100%',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+	backgroundColor: '#CFE9E5',
+	padding: 15,
+	gap: 30,
+	},
+
+	header_text: {
+		fontSize:24,
+		fontWeight:'bold',
+
+	},
+	layout: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 5,
+	},
+
+	input:{
+		height: 30,
+		width: '75%',
+		fontSize: 18,
+	},
+	button:{
+		flexDirection: 'row',
+		gap: 10,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height:60,
+		width: '60%',
+		padding:15,
+		borderRadius: 3,
+		elavation: 8,
+		shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+	},
+	button_entrar: {
+    backgroundColor: '#87C9BF',
+		marginBottom: 70,
+		marginTop: 40,
+  },
+  button_facebook: {
+    backgroundColor: '#1976D2',
+  },
+  button_google: {
+    backgroundColor: '#E57373', 
+  },
+
+});
